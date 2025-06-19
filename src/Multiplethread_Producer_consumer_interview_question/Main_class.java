197f1446022de7194236c596f460d49d702f17c9
Rishav kumar jain <rishavkumarjain2000@gmail.com>
2025-06-19 11:23:46 +0530

    updated

---
+++ src/Multiplethread_Producer_consumer_interview_question/Main_class.java
@@ -0,0 +1,29 @@
+package Multiplethread_Producer_consumer_interview_question;
+
+public class Main_class {
+    public static void main(String args[]){
+        SharedResources sharedResources=new SharedResources(5);
+        Thread producerthread=new Thread(()->{
+            try{
+                for(int i=1;i<8;i++){
+
+                    sharedResources.produce(i);
+                }
+            }catch (Exception e){
+
+            }
+        });
+
+        Thread consumerthread=new Thread(()->{
+            try{
+                for(int i=1;i<8;i++){
+                    sharedResources.consumer(i);
+                }
+            }catch (Exception e){
+
+            }
+        });
+        producerthread.start();
+        consumerthread.start();
+    }
+}
---
+++ src/Multiplethread_Producer_consumer_interview_question/SharedResources.java
@@ -0,0 +1,36 @@
+package Multiplethread_Producer_consumer_interview_question;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SharedResources {
+    private int buffersize;
+    private Queue<Integer>bufferq;
+
+    public SharedResources(int buffersize){
+        bufferq=new LinkedList<>();
+        this.buffersize=buffersize;
+    }
+    public synchronized void produce(int item) throws Exception{
+//        System.out.println("Item need to added : 1st block of producer");
+        while (bufferq.size()==buffersize){
+            System.out.println("Buffer queue is full,Producer is wait for consumer ");
+            wait();
+        }
+        bufferq.add(item);
+        System.out.println("Added: " + item);
+        notifyAll();
+    }
+    public synchronized int consumer(int i)throws Exception{
+        while(bufferq.isEmpty()){
+            System.out.println("Buffer is empty now : ");
+            wait();
+        }
+        int item=bufferq.poll();
+        System.out.println("Item is Consumed : "+ item);
+
+        notifyAll();
+        return item;
+    }
+
+}
